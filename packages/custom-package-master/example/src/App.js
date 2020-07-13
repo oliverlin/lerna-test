9a8e601982887e7fc947b70bd6db68a73ab5960b
Oliver Lin <linchong223@gmail.com>
2020-07-13 18:13:11 +0800

    Update master script and code

--- packages/custom-package-master/example/src/App.js
+++ packages/custom-package-master/example/src/App.js
@@ -4,7 +4,7 @@ import { ExampleComponent } from 'custom-package-master'
 import 'custom-package-master/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  return <ExampleComponent text="XCreate React Library Example 😄" />
 }
 
 export default App
