be8e57ecd23e293b426196ec1ad7a7b23331179f
Oliver Lin <linchong223@gmail.com>
2020-07-13 15:14:21 +0800

    Include custom comp

--- packages/custom-package-master/src/index.js
+++ packages/custom-package-master/src/index.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import styles from './styles.module.css'
+import { ExampleComponent as Me } from 'custom-react-package'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+  return <div className={styles.test}>
+    Example Component: {text}
+    <Me text={"it's custom"}/>
+  </div>
 }
