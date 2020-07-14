2b223978a70416f837b3f43f4bd3e413f6131d15
Oliver Lin <linchong223@gmail.com>
2020-07-14 13:28:11 +0800

    Tweak child package

--- packages/custom-react-package/src/index.js
+++ packages/custom-react-package/src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Cool Hello: {text}</div>
+  return <div className={styles.test}>Cool2 Hello: {text}</div>
 }
