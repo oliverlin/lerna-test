68707b5523b7759be142342f4221d9c7626a40d9
Oliver Lin <linchong223@gmail.com>
2020-07-23 13:35:22 +0800

    Tweak wording

--- packages/custom-react-package/src/index.js
+++ packages/custom-react-package/src/index.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './styles.module.css'
 
 export const ExampleComponent = ({ text }) => {
-  return <div className={styles.test}>Cool4 Hello: {text}</div>
+  return <div className={styles.test}>Cool5 Hello: {text}</div>
 }
